880968fd5b2971792f14678cf9c95f972daa93fa
Virgínia Arruda <vica_arruda@hotmail.com>
2021-04-27 07:55:17 -0300

    Corrige console.log do else

--- javascript/ex013/ambiente.js
+++ javascript/ex013/ambiente.js
@@ -4,9 +4,9 @@ num.push(1)
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
-let pos = num.indexOf(4)
+let pos = num.indexOf(8)
 if (pos == -1) {
   console.log('O valor não foi encontrado!')
 } else {
-  console.log(`O valor 8 está na posição ${pos}`)
+  console.log(`O valor está na posição ${pos}`)
 }
