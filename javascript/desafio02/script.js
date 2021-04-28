e1906ec5bf5002c46a0a2be401942a8c6f9d5656
Virgínia Arruda <vica_arruda@hotmail.com>
2021-04-28 10:26:47 -0300

    Verificador de idade

--- javascript/desafio02/script.js
+++ javascript/desafio02/script.js
@@ -1,21 +1,50 @@
-function carregar() {
-  let msg = document.getElementById('msg')
-  let img = document.getElementById('imagem')
+function verificar() {
   let data = new Date()
-  //let hora = data.getHours()
-  let hora = 19
-  msg.innerHTML = `Agora são ${hora} horas.`
-  if (hora >= 0 && hora < 12) {
-    //Bom dia!
-    img.src = 'imagens/manha.jpg'
-    document.body.style.background = '#D6BC2B'
-  } else if (hora >= 12 && hora <= 18) {
-    //Boa tarde!
-    img.src = 'imagens/tarde.jpg'
-    document.body.style.background = '#FAAE71'
+  let ano = data.getFullYear()
+  let fano = document.getElementById('txtano')
+  let res = document.getElementById('res')
+  if (fano.value.length == 0 || Number(fano.value) > ano) {
+    alert('[ERRO] Verifique os dados e tente novamente!')
   } else {
-    //Boa noite!
-    img.src = 'imagens/noite.jpg'
-    document.body.style.background = '#100021'
+    let fsex = document.getElementsByName('radsex')
+    let idade = ano - Number(fano.value)
+    let gênero = ''
+    let img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+        genero = 'Homem'
+        if (idade >= 0 && idade < 10) {
+          //criança
+          img.setAttribute('src', 'imagens/menino.jpg')
+        } else if (idade < 21) {
+          //jovem
+          img.setAttribute('src', 'imagens/adolescentehomem.jpg')
+        } else if (idade < 50) {
+          //Adulto
+          img.setAttribute('src', 'imagens/adultohomem.jpg')
+        } else {
+          //idoso
+          img.setAttribute('src', 'imagens/idoso.jpg')
+        }
+    } else if (fsex[1].checked) {
+        genero = 'Mulher'
+        if (idade >= 0 && idade < 10) {
+          //criança
+          img.setAttribute('src', 'imagens/menina.jpg')
+        } else if (idade < 21) {
+          //jovem
+          img.setAttribute('src', 'imagens/adolescentemulher.jpg')
+        } else if (idade < 50) {
+          //Adulto
+          img.setAttribute('src', 'imagens/adultomulher.jpg')
+        } else {
+          //idoso
+          img.setAttribute('src', 'imagens/idosa.jpg')
+        }
+    }
+    foto.style.borderRadius = "100%";
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+    res.appendChild(img)
   }
 }
